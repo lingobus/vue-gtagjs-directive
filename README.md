bd7303a4665eca8ebb18b9f12e377e6eea68a4ef
wangweihua <aztack@163.com>
2017-12-06 18:06:49 +0800

    use binding.arg instead of binding.modifiers

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # vue-gtag-directive
 
-# NOT TESTED, DO NOT USE!
-
 Vue directive for Google gtag.js
 
 Install
@@ -13,13 +11,20 @@ Usage
 
 First, include gtag.js in you page or use [vue-gtagjs](https://github.com/lingobus/vue-gtagjs)
 
+```js
+import gtagHelper from 'vue-gtagjs-directive'
+gtagHelper.setTrackId('UA-XXXXXXXX-1')
+```
 
 ## gtag('config',GA_TRACK_ID, {page_path:'/path'})
 ```pug
-button(v-gtag.config="{page_path:'/path'}", track-id="UA-XXXXXXXX-1", trigger-event="mouseup") test
+//- if you have setTrackId, track-id attribute can be omitted
+button(v-gtag.config="{page_path:'/path'}", trigger-event="mouseup", :before-send="beforeSend", :after-send="afterSend") test
+//- otherwise, track-id is mandatory
+button(v-gtag:config="{page_path:'/path'}", track-id="UA-XXXXXXXX-1", trigger-event="mouseup") test
 ```
 
 ## gtag('event',EVENT_NAME, {event_category:'CATA','event_label':'Label'})
 ```pug
-button(v-gtag.event="{event_category:'CATA','event_label':'Label'}", event-name="EVENT-NAME") test
+button(v-gtag:event="{event_category:'CATA','event_label':'Label'}", event-name="EVENT-NAME") test
 ```
--- index.js
+++ index.js
@@ -1,36 +1,66 @@
 /* globals Vue */
+var trackId = null
 Vue.directive('gtag', {
   bind: function(el, binding, vnode) {
     init()
+
+    // collect arguments
     const vattrs = vnode.data.attrs || {}
-    var act, attr
-    if (binding.modifiers.event) {
-      act = 'event'
-      attr = 'event-name'
-    } else if (binding.modifiers.config) {
-      act = 'config'
+    var act = binding.arg, val
+    if (act === 'event') {
+      val = vnode.data.attrs['event-name']
+    } else if (act == 'config') {
       attr = 'track-id'
+      val = trackId || vnode.data.attrs['track-id']
+      if (!val) throw new Error('Please set track id first!')
+    } else {
+      return
     }
-    const val = vnode.data.attrs[attr]
+
     const triggerEvent = vattrs['trigger-event'] || 'click'
     const beforeSend = vattrs['before-send'] || noop
     const afterSend = vattrs['after-send'] || noop
+
+    // add event listener
     el.addEventListener(triggerEvent, e => {
       const ret = beforeSend(e, el, binding, vnode)
-      if (ret === false) return
-      gtag(act, val, binding.value)
-      afterSend(e, el, binding, vnode)
+      if (ret === false) {
+        // cancel calling gtag if beforeSend return false
+        return
+      } else if (ret && typeof ret.then === 'function') {
+        // beforeSend returns a promise
+        ret.then(_ => {
+          gtag(act, val, binding.value, _)
+          afterSend(e, el, binding, vnode)
+        }).catch(_ => _)
+      } else {
+        // otherwise, call gtag right away
+        gtag(act, val, binding.value)
+        afterSend(e, el, binding, vnode)
+      }
     })
   }
 })
 
 function noop () {}
 
+/**
+ * create stub if gtag.js is not loaded
+ */
 function init() {
   const w = window
   if (w.gtag) return
   w.dataLayer = w.dataLayer || []
   w.gtag = function() {
     w.dataLayer.push(arguments)
+  }
+}
+
+export default {
+  setTrackId (id) {
+    trackId = id
+  },
+  getTrackId () {
+    return trackId
   }
 }
